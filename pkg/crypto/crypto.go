def7189713c4062b12ea9ee7f55d3a9fe658408a
Yad Smood <y.s.inside@gmail.com>
2019-09-24 19:02:47 +0900

    up

--- pkg/crypto/crypto.go
+++ pkg/crypto/crypto.go
@@ -8,66 +8,98 @@ import (
 	"crypto/rsa"
 	"crypto/sha256"
 	"crypto/x509"
+	"encoding/json"
 	"encoding/pem"
 	"io"
 )
 
+// PrivateKey ...
 type PrivateKey struct {
 	hash    []byte
 	private *rsa.PrivateKey
+	public  *PublicKey
 }
 
-type PublicKey struct {
-	hash   []byte
-	public *rsa.PublicKey
-}
-
+// NewPrivateKey ...
 func NewPrivateKey(file []byte) (*PrivateKey, error) {
-	key32 := sha256.Sum256(file)
 	key, err := parsePrivateKey(file)
 	if err != nil {
 		return nil, err
 	}
 
-	return &PrivateKey{
-		hash:    key32[0:],
-		private: key,
-	}, nil
-}
-
-func NewPublicKey(file []byte) (*PublicKey, error) {
-	key32 := sha256.Sum256(file)
-	key, err := parsePublicKey(file)
+	privateHash, err := hash(key)
 	if err != nil {
 		return nil, err
 	}
 
-	return &PublicKey{
-		hash:   key32[0:],
-		public: key,
+	publicHash, err := hash(&key.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PrivateKey{
+		hash:    privateHash,
+		private: key,
+		public: &PublicKey{
+			hash:   publicHash,
+			public: &key.PublicKey,
+		},
 	}, nil
 }
 
+// Encrypt ...
 func (key *PrivateKey) Encrypt(plaintext []byte) ([]byte, error) {
 	return encrypt(key.hash, plaintext)
 }
 
+// Decrypt ...
 func (key *PrivateKey) Decrypt(ciphertext []byte) ([]byte, error) {
 	return decrypt(key.hash, ciphertext)
 }
 
+// Sign ...
 func (key *PrivateKey) Sign(data []byte) ([]byte, error) {
 	hash := sha256.Sum256(data)
 
 	return rsa.SignPKCS1v15(rand.Reader, key.private, crypto.SHA256, hash[0:])
 }
 
+// Verify ...
+func (key *PrivateKey) Verify(data []byte, sig []byte) error {
+	return key.public.Verify(data, sig)
+}
+
+// PublicKey ...
+type PublicKey struct {
+	hash   []byte
+	public *rsa.PublicKey
+}
+
+// NewPublicKey ...
+func NewPublicKey(file []byte) (*PublicKey, error) {
+	key, err := parsePublicKey(file)
+	if err != nil {
+		return nil, err
+	}
+
+	publicHash, err := hash(key)
+	if err != nil {
+		return nil, err
+	}
+	return &PublicKey{
+		hash:   publicHash,
+		public: key,
+	}, nil
+}
+
+// Verify ...
 func (key *PublicKey) Verify(data []byte, sig []byte) error {
 	hash := sha256.Sum256(data)
 
 	return rsa.VerifyPKCS1v15(key.public, crypto.SHA256, hash[0:], sig)
 }
 
+// parsePrivateKey ...
 func parsePrivateKey(privateKey []byte) (*rsa.PrivateKey, error) {
 	block, _ := pem.Decode(privateKey)
 
@@ -84,6 +116,19 @@ func parsePrivateKey(privateKey []byte) (*rsa.PrivateKey, error) {
 	return i.(*rsa.PrivateKey), nil
 }
 
+// hash ...
+func hash(key interface{}) ([]byte, error) {
+	data, err := json.Marshal(key)
+	if err != nil {
+		return nil, err
+	}
+
+	hash := sha256.Sum256(data)
+
+	return hash[0:], nil
+}
+
+// parsePublicKey ...
 func parsePublicKey(publicKey []byte) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode(publicKey)
 
@@ -100,6 +145,7 @@ func parsePublicKey(publicKey []byte) (*rsa.PublicKey, error) {
 	return i.(*rsa.PublicKey), nil
 }
 
+// encrypt ...
 func encrypt(key []byte, plaintext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -121,6 +167,7 @@ func encrypt(key []byte, plaintext []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
+// decrypt ...
 func decrypt(key, ciphertext []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
