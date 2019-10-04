a0da6134ef2ff981f28524a6b89df136af3f6a47
Yad Smood <y.s.inside@gmail.com>
2019-10-04 19:21:47 +0900

    up

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/ysmood/openkv
 go 1.13
 
 require (
+	github.com/gorilla/websocket v1.4.1
 	github.com/vmihailenco/msgpack/v4 v4.2.0
 	github.com/ysmood/byframe v1.1.1
 	github.com/ysmood/kit v0.16.0
+	github.com/ysmood/storer v0.0.1
 )
--- server.go
+++ server.go
@@ -1,16 +1,36 @@
 package openkv
 
 import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
 	"github.com/ysmood/kit"
+	"github.com/ysmood/storer/pkg/kvstore"
 )
 
-// Server ...
-type Server struct {
+// ServerHandler ...
+type ServerHandler func(http.ResponseWriter, *http.Request)
+
+type txn struct {
+	db       kvstore.Store
+	upgrader *websocket.Upgrader
 }
 
-// NewServer ...
-func NewServer() *Server {
-	kit.Log("ok")
+// Serve ...
+func Serve(db kvstore.Store, upgrader *websocket.Upgrader) ServerHandler {
+	t := txn{db, upgrader}
 
-	return &Server{}
+	return t.Handler
+}
+
+func (t *txn) Handler(w http.ResponseWriter, r *http.Request) {
+	c, err := t.upgrader.Upgrade(w, r, nil)
+	kit.E(err)
+	defer c.Close()
+}
+
+func (t *txn) exec(c *websocket.Conn) {
+	for {
+		c.ReadMessage()
+	}
 }
